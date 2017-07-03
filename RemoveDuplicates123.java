4cfc25f4815d3d394882c63b147781a8e58397e1
ManikandanRamakrishnan <manikandanrk97@gmail.com>
2017-07-03 15:28:52 +0530

    Add files via upload

---
+++ RemoveDuplicates123.java
@@ -0,0 +1,27 @@
+package GUVI;
+
+import java.util.Scanner;
+
+public class RemoveDuplicates123 {
+
+	public static void main(String[] args) {
+
+		Scanner input=new Scanner(System.in);
+		String str=input.nextLine();
+		for(int i=0;i<str.length();i++){
+			boolean unique=true;
+			for(int j=0;j<str.length();j++){
+				if(i!=j){
+					if(str.charAt(i)==str.charAt(j)){
+						unique=false;	
+					}
+				}
+			}
+			if(unique){
+				System.out.print(str.charAt(i));
+			}
+		}
+		input.close();
+	}
+
+}
