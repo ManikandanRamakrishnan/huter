79933e142030e509e3ce4f84c703915870a3dd9b
ManikandanRamakrishnan <manikandanrk97@gmail.com>
2017-07-03 15:36:48 +0530

    Update RemoveDulpicate28.java

--- RemoveDulpicate28.java
+++ RemoveDulpicate28.java
@@ -1,26 +1,18 @@
 package GUVI;
 
-
+import java.util.HashSet;
 import java.util.Scanner;
 public class RemoveDulpicate28 {
 	public static void main(String[] args) {
-
 		Scanner input=new Scanner(System.in);
 		String str=input.nextLine();
+		HashSet<Character> uniqueString=new HashSet<Character>();
 		for(int i=0;i<str.length();i++){
-			boolean unique=true;
-			for(int j=0;j<str.length();j++){
-				if(i!=j){
-					if(str.charAt(i)==str.charAt(j)){
-						unique=false;	
-					}
-				}
-			}
-			if(unique){
+			if(uniqueString.add(str.charAt(i))){
 				System.out.print(str.charAt(i));
 			}
 		}
-		input.close();
+input.close();
 	}
 
 }
